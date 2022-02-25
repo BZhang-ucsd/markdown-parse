ba15ac02109b39c842fa18700e9e4045d471a2f1
BZhang-ucsd <boz002@ucsd.edu>
2022-02-25 12:26:58 -0800

    1

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -13,6 +13,39 @@ public class MarkdownParseTest{
         assertEquals(2, 1 + 1);
     }
 
+    @Test
+    public void testGetLinks1() throws IOException{
+        String file=Files.readString(Path.of("test-file.md"));
+        assertEquals(MarkdownParse.getLinks(file), List.of("https://something.com","some-page.html"));
+    }
+    @Test
+    public void testGetLinks2() throws IOException{
+        String file=Files.readString(Path.of("test-file2.md"));
+        assertEquals(MarkdownParse.getLinks(file), 
+        List.of("https://www.youtube.com/watch?v=dQw4w9WgXcQ","https://www.google.com/maps"));
+    }
+    @Test
+    public void testGetLinks3() throws IOException{
+        String file=Files.readString(Path.of("test-file3.md"));
+        assertEquals(MarkdownParse.getLinks(file), 
+        List.of("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+    }
+    @Test
+    public void testGetLinksBulk() throws IOException{
+        String file=Files.readString(Path.of("test-file4.md"));
+        assertEquals(MarkdownParse.getLinks(file),List.of());
+        file=Files.readString(Path.of("test-file6.md"));
+        assertEquals(MarkdownParse.getLinks(file),List.of());
+        file=Files.readString(Path.of("test-file7.md"));
+        assertEquals(MarkdownParse.getLinks(file),List.of());
+        //tests below are bugged
+        file=Files.readString(Path.of("test-file8.md"));
+        assertEquals(MarkdownParse.getLinks(file),List.of("a link on the first line"));
+        file=Files.readString(Path.of("test-file5.md"));
+        assertEquals(MarkdownParse.getLinks(file),List.of());
+    }
+
+
 /** @Test 
 public void testgetLinks1() throws IOException {
 
